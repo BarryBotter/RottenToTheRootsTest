8263f0bb5cae42e2302b86168019463582f2c1d6
Jimi Björkbacka <t6bjji00@students.oamk.fi>
2018-03-12 14:46:15 +0200

    jeee

--- app/src/main/java/hammas/peli/MainActivity.java
+++ app/src/main/java/hammas/peli/MainActivity.java
@@ -1,7 +1,9 @@
 package hammas.peli;
 
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    protected void onClick(View v)
+    {
+        
     }
 }
